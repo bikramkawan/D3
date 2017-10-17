3d7690b7367820231f41f7bebaa22ca67501ba0f
bikramkawan <bikramkawan@gmail.com>
2017-10-17 21:50:53 +0200

    updating charts with new data

--- StackedChart/data.js
+++ StackedChart/data.js
@@ -99,3 +99,579 @@ var activities = [{
     date: "25-Apr-12",
     value: "2"
 }];
+
+
+var newdata = {
+    "glucose": [
+        {
+            "source": "ot5bt",
+            "created": "02.10.17 06:58",
+            "value": 111,
+            "type": "control",
+            "comment": "gerät getestet"
+        },
+        {
+            "source": "ot5bt",
+            "created": "02.10.17 07:00",
+            "value": 90,
+            "type": "preprandial",
+            "comment": "noch kein hunger"
+        },
+        {
+            "source": "ot5bt",
+            "created": "02.10.17 10:00",
+            "value": 45,
+            "type": "preprandial",
+            "comment": "jetzt ist mir schon schlecht vor hunger"
+        },
+        {
+            "source": "ot5bt",
+            "created": "02.10.17 12:30",
+            "value": 120,
+            "type": "preprandial"
+        },
+        {
+            "source": "ot5bt",
+            "created": "02.10.17 12:30",
+            "value": 444,
+            "type": "invalid",
+            "comment": "noch nutella am finger gehabt"
+        },
+        {
+            "source": "ot5bt",
+            "created": "02.10.17 13:30",
+            "value": 250,
+            "type": "postprandial"
+        },
+        {
+            "source": "ot5bt",
+            "created": "02.10.17 15:05",
+            "value": 105,
+            "type": "none"
+        },
+        {
+            "source": "ot5bt",
+            "created": "02.10.17 18:02",
+            "value": 450,
+            "type": "invalid",
+            "comment": "hände vorher nicht gewaschen"
+        },
+        {
+            "source": "web",
+            "created": "02.10.17 18:02",
+            "value": 75,
+            "type": "preprandial"
+        },
+        {
+            "source": "app",
+            "created": "02.10.17 19:45",
+            "value": 133,
+            "type": "postprandial"
+        },
+        {
+            "source": "app",
+            "created": "02.10.17 23:15",
+            "value": 124,
+            "type": "generic",
+            "comment": "vor schlafengehen"
+        },
+        {
+            "source": "ot5bt",
+            "created": "03.10.17 08:58",
+            "value": 111,
+            "type": "control",
+            "comment": "gerät getestet"
+        },
+        {
+            "source": "ot5bt",
+            "created": "03.10.17 09:00",
+            "value": 80,
+            "type": "preprandial",
+            "comment": "noch kein hunger"
+        },
+        {
+            "source": "ot5bt",
+            "created": "03.10.17 10:00",
+            "value": 60,
+            "type": "preprandial",
+            "comment": "mir ist schon schlecht vor hunger"
+        },
+        {
+            "source": "ot5bt",
+            "created": "03.10.17 12:30",
+            "value": 110,
+            "type": "preprandial"
+        },
+        {
+            "source": "ot5bt",
+            "created": "03.10.17 13:45",
+            "value": 315,
+            "type": "postprandial"
+        },
+        {
+            "source": "ot5bt",
+            "created": "03.10.17 15:05",
+            "value": 105,
+            "type": "none",
+            "comment": "nur mal so zwischendrin"
+        },
+        {
+            "source": "ot5bt",
+            "created": "03.10.17 17:55",
+            "value": 30,
+            "type": "invalid",
+            "comment": "streifen nicht i.o."
+        },
+        {
+            "source": "web",
+            "created": "03.10.17 17:55",
+            "value": 75,
+            "type": "preprandial"
+        },
+        {
+            "source": "app",
+            "created": "03.10.17 19:45",
+            "value": 202,
+            "type": "postprandial"
+        },
+        {
+            "source": "app",
+            "created": "03.10.17 23:15",
+            "value": 143,
+            "type": "generic",
+            "comment": "vor schlafengehen"
+        },
+        {
+            "source": "ot5bt",
+            "created": "04.10.17 06:58",
+            "value": 111,
+            "type": "control",
+            "comment": "gerät getestet"
+        },
+        {
+            "source": "ot5bt",
+            "created": "04.10.17 07:00",
+            "value": 160,
+            "type": "preprandial",
+            "comment": "noch kein hunger"
+        },
+        {
+            "source": "ot5bt",
+            "created": "04.10.17 12:30",
+            "value": 260,
+            "type": "preprandial"
+        },
+        {
+            "source": "ot5bt",
+            "created": "04.10.17 12:30",
+            "value": 580,
+            "type": "invalid",
+            "comment": "noch nutella am finger gehabt"
+        },
+        {
+            "source": "ot5bt",
+            "created": "04.10.17 13:30",
+            "value": 115,
+            "type": "postprandial"
+        },
+        {
+            "source": "web",
+            "created": "04.10.17 18:02",
+            "value": 49,
+            "type": "preprandial"
+        },
+        {
+            "source": "app",
+            "created": "04.10.17 19:45",
+            "value": 65,
+            "type": "postprandial"
+        },
+        {
+            "source": "app",
+            "created": "04.10.17 23:15",
+            "value": 118,
+            "type": "generic",
+            "comment": "vor schlafengehen"
+        }
+    ],
+    "meal": [
+        {
+            "source": "app",
+            "created": "02.10.17 08:00",
+            "value": 20,
+            "type": "breakfast",
+            "comment": "apfel"
+        },
+        {
+            "source": "app",
+            "created": "02.10.17 10:35",
+            "value": 15,
+            "type": "snack",
+            "comment": "traubenzucker"
+        },
+        {
+            "source": "app",
+            "created": "02.10.17 12:30",
+            "value": 80,
+            "type": "lunch",
+            "comment": "nudeln mit tomatensoße"
+        },
+        {
+            "source": "web",
+            "created": "02.10.17 18:30",
+            "value": 35,
+            "type": "dinner",
+            "comment": "käsebrot"
+        },
+        {
+            "source": "web",
+            "created": "02.10.17 21:30",
+            "value": 10,
+            "type": "snack",
+            "comment": "joghurt"
+        },
+        {
+            "source": "app",
+            "created": "03.10.17 08:00",
+            "value": 8,
+            "type": "breakfast",
+            "comment": "apfel"
+        },
+        {
+            "source": "app",
+            "created": "03.10.17 10:35",
+            "value": 20,
+            "type": "snack",
+            "comment": "honig"
+        },
+        {
+            "source": "app",
+            "created": "03.10.17 12:30",
+            "value": 110,
+            "type": "lunch",
+            "comment": "pizza"
+        },
+        {
+            "source": "web",
+            "created": "03.10.17 18:30",
+            "value": 20,
+            "type": "dinner",
+            "comment": "gemüsepfanne"
+        },
+        {
+            "source": "app",
+            "created": "04.10.17 10:35",
+            "value": 15,
+            "type": "snack",
+            "comment": "apfel"
+        },
+        {
+            "source": "app",
+            "created": "04.10.17 12:30",
+            "value": 38,
+            "type": "lunch",
+            "comment": "salat mit putenbrust und brot"
+        },
+        {
+            "source": "web",
+            "created": "04.10.17 18:30",
+            "value": 10,
+            "type": "dinner",
+            "comment": "gemüsesuppe"
+        }
+    ],
+    "activity": [
+        {
+            "source": "app",
+            "created": "02.10.17 08:30",
+            "duration": 1,
+            "intensity": "strong",
+            "type": "jog",
+            "comment": "alter, war das anstrengend"
+        },
+        {
+            "source": "web",
+            "created": "02.10.17 13:00",
+            "duration": 0.75,
+            "intensity": "light",
+            "type": "walking",
+            "comment": "mittagspause, parkrunde"
+        },
+        {
+            "source": "app",
+            "created": "02.10.17 19:45",
+            "duration": 1.25,
+            "intensity": "medium",
+            "type": "fitness_center",
+            "comment": "bodypump"
+        },
+        {
+            "source": "web",
+            "created": "03.10.17 13:00",
+            "duration": 0.75,
+            "intensity": "medium",
+            "type": "walking",
+            "comment": "mittagspause, parkrunde"
+        },
+        {
+            "source": "app",
+            "created": "03.10.17 19:45",
+            "duration": 1.25,
+            "intensity": "strong",
+            "type": "fitness_center",
+            "comment": "bodypump"
+        },
+        {
+            "source": "app",
+            "created": "04.10.17 08:30",
+            "duration": 1,
+            "intensity": "medium",
+            "type": "jog",
+            "comment": "alter, war das anstrengend"
+        },
+        {
+            "source": "web",
+            "created": "04.10.17 13:00",
+            "duration": 0.75,
+            "intensity": "light",
+            "type": "walking",
+            "comment": "mittagspause, parkrunde"
+        }
+    ],
+    "medication": [
+        {
+            "source": "app",
+            "created": "02.10.17 07:00",
+            "value": 5,
+            "name": "lantus",
+            "type": "basis_insulin",
+            "comment": "wie immer morgens"
+        },
+        {
+            "source": "app",
+            "created": "02.10.17 08:25",
+            "value": 3,
+            "name": "novorapid",
+            "type": "bolus_insulin",
+            "comment": "nach breakfast"
+        },
+        {
+            "source": "web",
+            "created": "02.10.17 10:35",
+            "value": 2,
+            "name": "mix_insulin",
+            "type": "mix_insulin"
+        },
+        {
+            "source": "web",
+            "created": "02.10.17 11:11",
+            "value": 1,
+            "name": "aspirin",
+            "type": "other_medication",
+            "comment": "kopfschmerzen"
+        },
+        {
+            "source": "app",
+            "created": "02.10.17 12:30",
+            "value": 6,
+            "name": "novorapid",
+            "type": "bolus_insulin",
+            "comment": "zum lunch"
+        },
+        {
+            "source": "app",
+            "created": "02.10.17 23:00",
+            "value": 5,
+            "name": "lantus",
+            "type": "basis_insulin",
+            "comment": "wie immer abends"
+        },
+        {
+            "source": "app",
+            "created": "03.10.17 07:00",
+            "value": 5,
+            "name": "lantus",
+            "type": "basis_insulin",
+            "comment": "wie immer morgens"
+        },
+        {
+            "source": "app",
+            "created": "03.10.17 12:30",
+            "value": 3,
+            "name": "novorapid",
+            "type": "bolus_insulin",
+            "comment": "zum lunch"
+        },
+        {
+            "source": "app",
+            "created": "03.10.17 23:00",
+            "value": 5,
+            "name": "lantus",
+            "type": "basis_insulin",
+            "comment": "wie immer abends"
+        },
+        {
+            "source": "app",
+            "created": "04.10.17 07:00",
+            "value": 5,
+            "name": "lantus",
+            "type": "basis_insulin",
+            "comment": "wie immer morgens"
+        },
+        {
+            "source": "app",
+            "created": "04.10.17 08:25",
+            "value": 10,
+            "name": "novorapid",
+            "type": "bolus_insulin",
+            "comment": "nach breakfast"
+        },
+        {
+            "source": "web",
+            "created": "04.10.17 10:35",
+            "value": 2,
+            "name": "mix_insulin",
+            "type": "mix_insulin"
+        },
+        {
+            "source": "web",
+            "created": "04.10.17 11:11",
+            "value": 1,
+            "name": "aspirin",
+            "type": "other_medication",
+            "comment": "kopfschmerzen"
+        },
+        {
+            "source": "app",
+            "created": "04.10.17 12:30",
+            "value": 7,
+            "name": "novorapid",
+            "type": "bolus_insulin",
+            "comment": "zum lunch"
+        },
+        {
+            "source": "app",
+            "created": "04.10.17 23:00",
+            "value": 5,
+            "name": "lantus",
+            "type": "basis_insulin",
+            "comment": "wie immer abends"
+        }
+    ]
+}
+
+
+var limits = [{
+    label: 'hyper',
+    value: 550
+},
+    {
+        label: 'target',
+        value: 250
+    },
+
+    {
+        label: 'hypo',
+        value: 50
+    }
+
+];
+
+
+var greenAreaLimits = [{
+    label: 'upper',
+    value: 490
+}, {
+    label: 'lower',
+    value: 120
+}]
+
+
+function formatData() {
+    var formatTime = d3.timeParse("%d.%m.%y %H:%M");
+
+    const glucoseNew = newdata.glucose;
+    const meal = newdata.meal;
+    const activity = newdata.activity;
+    const medication = newdata.medication;
+
+
+// format the data
+    const lineData = glucoseNew.map(function (d) {
+        return {
+            date: formatTime(d.created),
+            close: parseFloat(d.value)
+        }
+
+    });
+
+    const bardata = meal.map((d)=> {
+        return {
+            date: formatTime(d.created),
+            value: parseFloat(d.value)
+        }
+
+    });
+
+
+    const lightGreenData = medication.map((d)=> {
+        return {
+            date: formatTime(d.created),
+            value: parseFloat(d.value)
+        }
+
+    });
+
+    const orangeData = activity.map((d)=> {
+        return {
+            date: formatTime(d.created),
+            value: parseFloat(d.duration)
+        }
+
+    });
+
+
+    return {lineData, bardata, lightGreenData, orangeData}
+
+
+}
+
+
+function bottomBarHeight(d, height) {
+    return d <= 100 ? (height / 3) : (height / 3 + 20)
+
+}
+
+function calcGreenHeight(data, height) {
+
+// <10 : 1/27 of graph height
+// 10-19: 2/27 of graph height
+// >20 : 3/27 of graph height
+
+    if (data.value < 10) return (height / 27)
+    if (data.value >= 10 && data.value < 20) return ((2 / 27) * height)
+    if (data.value > 20) return ((3 / 27) * height)
+
+
+}
+
+
+function calcOrangeHeight(d, height) {
+
+// <10 : 1/27 of graph height
+// 10-19: 2/27 of graph height
+// >20 : 3/27 of graph height
+
+    if (d === 'light') return (height / 27)
+    if (d === 'medium') return ((2 / 27) * height)
+    if (d === 'strong') return ((3 / 27) * height)
+
+}
+
+function fillCircle(d) {
+
+
+    return (d >= greenAreaLimits[1].value && d < greenAreaLimits[0].value) ? 'green' : 'red'
+
+
+}
--- StackedChart/script.js
+++ StackedChart/script.js
@@ -1,12 +1,13 @@
-console.log(glucoses)
+console.log(newdata)
+const {lineData, bardata, lightGreenData, orangeData}  = formatData();
+
 
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-// parse the date / time
-var parseTime = d3.timeParse("%d-%b-%y");
-
+const bottomBarWidth = 10;
+const topBarWidth = 10;
 // set the ranges
 var x = d3.scaleTime().range([0, width]);
 var x2 = d3.scaleTime().range([0, width]);
@@ -44,42 +45,24 @@ svg.append("defs").append("clipPath")
     .attr("height", height);
 
 
-// format the data
-const data = glucoses.map(function (d) {
-    return {
-        date: parseTime(d.date),
-        close: parseFloat(d.value)
-    }
-
-});
-
-const bardata = meals.map((d)=> {
-    return {
-        date: parseTime(d.date),
-        value: parseFloat(d.value)
-    }
-
-});
-console.log(data, bardata)
-
 // Scale the range of the data
-x.domain(d3.extent(data, function (d) {
+x.domain(d3.extent(lineData, function (d) {
     return d.date;
 }));
 x2.domain(x.domain());
-// y.domain([0, d3.max(data, function (d) {
-//     return d.close;
-// })]);
+y.domain([0, d3.max(lineData, function (d) {
+    return d.close;
+})]);
 
 
-y.domain([0, 600]);
+// y.domain([0, 600]);
 
 const xAxis = d3.axisBottom(x),
     yAxis = d3.axisLeft(y);
 
 // Add the valueline path.
 svg.append("path")
-    .data([data])
+    .data([lineData])
     .attr("class", "line")
     .attr("d", valueline);
 
@@ -94,7 +77,16 @@ svg.append("g")
     .attr("class", "axis axis--y")
     .call(yAxis);
 
+const limitsLine = svg.append("g").classed('limits', true)
+const greenArea = d3.select('.limits')
+    .append('rect')
+    .classed('greenArea', true)
+    .attr('width', width)
+    .attr('height', y(greenAreaLimits[1].value) - y(greenAreaLimits[0].value))
+    .attr('x', 0)
+    .attr('y', y(greenAreaLimits[0].value))
 
+//Zoom Rect
 svg.append("rect")
     .attr("class", "zoom")
     .attr("width", width)
@@ -102,113 +94,57 @@ svg.append("rect")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
     .call(zoom);
 
-var barY = d3.scaleLinear().range([0, height / 3]).domain([0, d3.max(bardata, function (d) {
-    return d.value;
-})]);
+var barY = d3.scaleLinear()
+    .range([0, height / 3])
+    .domain([0, d3.max(bardata, function (d) {
+        return d.value;
+    })]);
 
-console.log(barY(bardata[4].value))
-const barRect = svg.append("g").classed('bar', true)
+
+const bottomBarGraph = svg.append("g").classed('bottomBar', true)
 // .attr("transform", "translate(0," + (height-barY.domain()[1]-50) + ")");
 
-barRect.selectAll('rect')
+
+bottomBarGraph.selectAll('rect')
     .data(bardata)
     .enter()
     .append('rect')
-    .attr('x', (d, i)=>(i + 1) * 100)
-    .attr('width', 50)
-    .attr('y', (d)=>height - barY(d.value))
-    .attr('height', (d)=>barY(d.value))
-    .attr('fill', 'grey')
+    .attr('x', (d, i)=>x(d.date))
+    .attr('width', bottomBarWidth)
+    .attr('y', (d, i)=>height - bottomBarHeight(d.value, height))
+    .attr('height', (d)=>bottomBarHeight(d.value, height))
 
 
-const lightGreenData = medications.map((d)=> {
-    return {
-        date: parseTime(d.date),
-        value: parseFloat(d.value)
-    }
-
-});
-
-const orangeData = activities.map((d)=> {
-    return {
-        date: parseTime(d.date),
-        value: parseFloat(d.value)
-    }
-
-});
-
-
-var yTop = d3.scaleLinear().range([0, height / 3]).domain([0, 120]);
-
-console.log(barY(bardata[4].value))
-const barTop = svg.append("g").classed('barTop', true)
+const topBarGreen = svg.append("g").classed('topBarGreen', true)
+const topBarOrange = svg.append("g").classed('topBarOrange', true)
 // .attr("transform", "translate(0," + (height-barY.domain()[1]-50) + ")");
 
-// <10  : 1/27 of graph height
-// 10-19: 2/27 of graph height
-// >20  : 3/27 of graph height
 
-function calcGreen(data) {
-
-    if (data.value < 10) return (120 / 4)
-    if (data.value > 10 && data.value < 20) return (120 / 3)
-    if (data.value > 20) return (120 / 2)
-
-
-}
-barTop.selectAll('rect')
+topBarGreen.selectAll('rect')
     .data(lightGreenData)
     .enter()
     .append('rect')
-    .attr('x', (d, i)=>(i + 1) * 100)
-    .attr('width', 50)
-    .attr('height', calcGreen)
-    .attr('fill', 'lightgreen')
+    .classed('topGreen', true)
+    .attr('x', (d, i)=>x(d.date))
+    .attr('width', topBarWidth)
+    .attr('height', (d)=>calcGreenHeight(d, height))
 
-const barOrange = svg.append("g").classed('barTop2', true)
-// 1: 1/27 of graph height
-// 2: 2/27 of graph height
-// 3: 3/27 of graph height
-barOrange.selectAll('rect')
+
+console.log(lightGreenData, orangeData)
+
+topBarOrange.selectAll('rect')
     .data(orangeData)
     .enter()
     .append('rect')
-    .attr('x', (d, i)=>(i + 1) * 100)
+    .classed('topOrange', true)
+    .attr('x', (d, i)=>x(d.date))
     .attr('width', 50)
-    .attr('y', (d, i)=> (calcGreen(lightGreenData[i])))
-    .attr('height', (d)=> {
-        if (d.value === 1) return (120 / 4)
-        if (d.value === 2) return (120 / 3)
-        if (d.value === 3) return (120 / 2)
+    .attr('y', (d, i)=> (calcGreenHeight(lightGreenData[i])))
+    .attr('height', (d)=> calcOrangeHeight(d.intensity, height))
 
-    })
-    .attr('fill', 'orange')
-
-
-var limits = [{
-    label: 'hyper',
-    value: 550
-},
-    {
-        label: 'upper',
-        value: 490
-    }, {
-        label: 'target',
-        value: 250
-    },
-    {
-        label: 'lower',
-        value: 120
-    }, {
-        label: 'hypo',
-        value: 50
-    }
-
-];
 
 var yPath = d3.scaleLinear().range([height, 0]).domain([0, 550]);
 
-const limitsLine = svg.append("g").classed('limits', true)
 
 limitsLine.selectAll('path')
     .data(limits)
@@ -218,25 +154,28 @@ limitsLine.selectAll('path')
     .attr('d', (d)=>`M0 ${y(d.value)} L ${width} ${y(d.value)}`)
     .attr('stroke', 'steelblue')
 
+console.log(height, y(greenAreaLimits[1].value), y(greenAreaLimits[0].value))
 
-// const circles = svg.append("g").classed('circle', true)
-//
-//
-// circles.selectAll('circle')
-//     .data(data)
-//     .enter()
-//     .append('circle')
-//     .attr('cx',(d)=>(d.close))
-//     .attr('cy',(d)=>y(d.close))
-//     .attr('r',4)
-//     .attr('stroke','red')
+
+const circles = svg.append("g").classed('circle', true)
+
+circles.selectAll('circle')
+    .data(lineData)
+    .enter()
+    .append('circle')
+    .attr('cx', (d)=>x(d.date))
+    .attr('cy', (d)=>y(d.close))
+    .attr('r', 4)
+    .attr('fill', (d)=>fillCircle(d.close))
 
 function zoomed() {
     const t = d3.event.transform;
-    console.log(t, t.rescaleX(x2).domain(), t.rescaleX(x2))
     x.domain(t.rescaleX(x2).domain());
     svg.select(".line").attr("d", valueline);
     svg.select(".axis--x").call(xAxis);
+    circles.selectAll('circle')
+        .attr('cx', (d)=> t.applyX(x2(d.date)))
+        .attr('cy', (d)=> y(d.close))
     // d3.select('.rectG')
     //     .attr("transform", `scale(${t.k})`)
     // .attr("transform", "translate(" + 100 + "," + 100 + ")")
