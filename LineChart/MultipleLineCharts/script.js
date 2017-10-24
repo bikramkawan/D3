b8130f4d9d3aa2cfa0f0c1e99a578680d0261845
bikramkawan <bikramkawan@gmail.com>
2017-10-24 23:50:20 +0200

    update charts with real data

--- LineChart/MultipleLineCharts/script.js
+++ LineChart/MultipleLineCharts/script.js
@@ -4,7 +4,7 @@
 
 
 const margin = {top: 20, right: 200, bottom: 50, left: 200},
-    width = 1000 - margin.left - margin.right,
+    width = 900 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 const svg = d3.select('.chart')
@@ -12,8 +12,8 @@ const svg = d3.select('.chart')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
 
-
-const parseDate = d3.timeParse("%b %Y");
+const colors = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628'];
+const parseDate = d3.timeParse("%m/%d/%y %H:%M:%S");
 const pt = d3.timeParse("%Y");
 
 const x = d3.scaleTime().range([0, width]),
@@ -28,73 +28,86 @@ const xAxis = d3.axisBottom(x),
     yAxis3 = d3.axisLeft(y3);
 
 
-const line1 = d3.line()
+const lineY11 = d3.line()
     .x((d)=> x(d.date))
-    .y((d)=>y1(d.price));
+    .y((d)=>y1(d.y11));
 
 
-const line2 = d3.line()
+const lineY12 = d3.line()
     .x((d)=> x(d.date))
-    .y((d)=> y2(d.price));
+    .y((d)=> y1(d.y12));
 
-
-const line3 = d3.line()
+const lineY13 = d3.line()
     .x((d)=> x(d.date))
-    .y((d)=>y3(d.price));
+    .y((d)=> y1(d.y13));
 
+const lineY21 = d3.line()
+    .x((d)=> x(d.date))
+    .y((d)=>y2(d.y21));
+
+
+const lineY22 = d3.line()
+    .x((d)=> x(d.date))
+    .y((d)=>y2(d.y22));
+
+
+const lineY31 = d3.line()
+    .x((d)=> x(d.date))
+    .y((d)=>y3(d.y31));
 
 const data = [];
 
-d3.csv("data.csv", function (error, rawdata) {
+d3.csv("newdata.csv", function (error, rawdata) {
+
+    console.log(rawdata)
 
     rawdata.forEach((item)=> {
-
         data.push({
-
-            date: parseDate(item.date),
-            price: parseFloat(item.price)
+            date: parseDate(`${item.date} ${item.time}`),
+            y11: parseFloat(item.y1_lp1),
+            y12: parseFloat(item.y1_lp2),
+            y13: parseFloat(item.y1_tp),
+            y21: parseFloat(item.y2_dr),
+            y22: parseFloat(item.y2_tr),
+            y31: parseFloat(item.y3_pc)
         })
 
 
     })
 
-
-    const line2Data = data.map(d=> {
-        return {
-            date: d.date,
-            price: d.price * Math.random()
-        }
-    });
-
-    const line3Data = data.map(d=> {
-        return {
-            date: d.date,
-            price: d.price * Math.random()
-        }
-    });
-
+    console.log(data)
 
     if (error) throw error;
+
+    let y1Extent = [0, 0];
+    let y2Extent = [0, 0];
+
+    data.forEach((d)=> {
+        const y1Values = d3.extent([d.y11, d.y12, d.y3])
+        const y2Values = d3.extent([d.y21, d.y22])
+        y1Extent = [Math.min(y1Values[0], y1Extent[0]), Math.max(y1Values[1], y1Extent[1])];
+        y2Extent = [Math.min(y2Values[0], y2Extent[0]), Math.max(y2Values[1], y2Extent[1])]
+
+    })
 
     x.domain(d3.extent(data, function (d) {
         return d.date;
     }));
-    y1.domain([0, d3.max(data, function (d) {
-        return d.price;
-    })]);
+    y1.domain(y1Extent);
 
-    y2.domain([0, d3.max(line2Data, function (d) {
-        return d.price;
-    })]);
+    y2.domain(y2Extent);
 
-    y3.domain([0, d3.max(line3Data, function (d) {
-        return d.price;
-    })]);
+    y3.domain(d3.extent(data, function (d) {
+        return d.y31;
+    }));
 
-    drawLine(data, line1, 'line1', yAxis1, 0);
-    drawLine(line2Data, line2, 'line2', yAxis2, -50);
-    drawLine(line3Data, line3, 'line3', yAxis3, -100);
-
+    console.log(y1.domain(), y2.domain(), y3.domain(), x.domain())
+    drawLine(data, lineY11, 'lineY11', yAxis1, 0);
+    drawLine(data, lineY12, 'lineY12', yAxis1, 0);
+    drawLine(data, lineY13, 'lineY13', yAxis1, 0);
+    drawLine(data, lineY21, 'lineY21', yAxis2, -50);
+    drawLine(data, lineY22, 'lineY22', yAxis2, -50);
+    drawLine(data, lineY31, 'lineY31', yAxis3, -100);
 
     const submit = d3.select('.submit').on('click', function (d) {
 
