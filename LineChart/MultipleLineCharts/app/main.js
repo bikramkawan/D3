36495b82e977aecddd340b9eef5add83c6dfdb6c
bikramkawan <bikramkawan@gmail.com>
2017-11-09 21:47:25 +0100

    moved to private repo

--- LineChart/MultipleLineCharts/app/main.js
+++
@@ -1,8 +0,0 @@
-define(function (require) {
-    // Load any app-specific modules
-    // with a relative require call,
-    // like:
-    var init = require('./initApp');
-
-    console.error(init.initApp())
-});
