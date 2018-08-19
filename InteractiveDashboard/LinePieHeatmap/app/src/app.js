21dbd0b8a40a1fbd4fe008e5c0ed07de887d566a
Bikram Kawan <bikramkawan@gmail.com>
2018-08-19 16:16:17 +0545

    make component reusable

--- InteractiveDashboard/LinePieHeatmap/app/src/app.js
+++ InteractiveDashboard/LinePieHeatmap/app/src/app.js
@@ -23,9 +23,91 @@ import axios from 'axios';
 
 const DEVICE_URL = 'https://ebs-granite.bigbang.io/api/v1/devices';
 
-axios.get(DEVICE_URL).then(response => {
-    console.error(response);
-});
+const beaconReaderId = 'device-id-8f373a31-a510-4a3f-9472-4a0f5bb56245';
+const timeInterval = 60;
+const startDateTime = '2018-08-09T05:00:00Z';
+const endDateTime = '2018-08-11T23:44:19Z';
+const intervalType = [
+    {
+        type: 'day',
+        timeInterval: 60,
+    },
+    {
+        type: 'week',
+        timeInterval: 10080,
+    },
+    {
+        type: 'month',
+        timeInterval: 43800,
+    },
+];
+function fetchDeviceData({ beaconReaderId, timeInterval, type }) {
+    return new Promise((resolve, reject) => {
+        axios({
+            method: 'post',
+            url: 'https://ebs-granite.bigbang.io/api/v1/call',
+            data: {
+                id: 'granite1',
+                namespace: '/deviceCountByTimeInterval',
+                message: {
+                    beaconReaderId,
+                    timeInterval,
+                    startDateTime,
+                    endDateTime,
+                },
+            },
+            headers: {
+                'Postman-Token': '9637ee41-e18d-4683-aabc-acd703e0ab94',
+                'Content-Type': 'application/json',
+                'Cache-Control': 'no-cache',
+                Authorization: 'Bearer at_4d114c8e-4867-414f-9d65-76c4a9b8bb0b',
+            },
+        })
+            .then(res => {
+                resolve({ data: res.data, type });
+            })
+            .catch(e => reject(e));
+    });
+}
+
+axios
+    .get(DEVICE_URL)
+    .then(response => {
+        console.error(response);
+        const devices = response.data && response.data.devices;
+
+        const promises = devices
+            .map(device =>
+                intervalType.map(it =>
+                    fetchDeviceData({
+                        beaconReaderId: device.id,
+                        timeInterval: 60,
+                        type: it.type,
+                    }),
+                ),
+            )
+            .reduce((a, b) => a.concat(b), []);
+        Promise.all(promises).then(response => {
+            console.log(response);
+            const day = response
+                .filter(r => r.type === 'day')
+                .filter(d => d.data.message.results.length > 0);
+            const week = response
+                .filter(r => r.type === 'week')
+                .filter(d => d.data.message.results.length > 0);
+            const month = response
+                .filter(r => r.type === 'month')
+                .filter(d => d.data.message.results.length > 0);
+
+            console.error(day, week, month);
+
+            initApp({day,week,month})
+        });
+        console.log(devices, 'devices  list', promises);
+    })
+    .catch(e => {
+        console.log(e, 'Error occured');
+    });
 
 d3
     .queue()
@@ -33,6 +115,16 @@ d3
     .defer(d3.json, 'data/new/getDeviceIdExample.json')
     .defer(d3.json, 'data/newdata.json')
     .awaitAll(ready);
+
+
+function initApp({day,week,month}) {
+
+
+
+
+}
+
+
 
 function ready(error, results) {
     console.error(results, 'dafafasjfalsf');
@@ -50,14 +142,10 @@ function ready(error, results) {
     const {
         width,
         height,
-        lineWidth,
-        lineHeight,
         heatWidth,
         heatHeight,
         pieElHeight,
         pieElWidth,
-        barWidth,
-        barHeight,
         margin,
         heatMapMargin,
     } = svg.getDimension();
